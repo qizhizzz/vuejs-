dbd998ee5344725335676854b3279cac76af23c3
qizhzzz <13498314+qizhzzz@user.noreply.gitee.com>
2024-09-30 17:07:42 +0800

    第二次作业

---
+++ vue-js-homework/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- 将组件添加 -->
+  <workTwo></workTwo>
+  <!-- 双向绑定inputData -->
+  <input type="text" v-model="inputData" placeholder="请输入">
+  <div >{{ inputData }}</div>
+  
+</template>
+
+<script setup>
+// 引入组件
+import workTwo from './components/workTwo.vue';
+import {ref} from 'vue'
+// 创建变量
+var inputData= ref()
+</script>
+
+
---
+++ vue-js-homework/src/components/workTwo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="height: 200px; width: 200px; background-color: skyblue">
+    这是个组件
+    <p :title="pTitle">{{pTitle}}</p>
+    <label>
+    <input type="radio" v-model="gender" name="gender" value=" 0"> 男
+  </label>
+  <label>
+    <input type="radio" v-model="gender" value="1"> 女
+  </label>
+  <div v-if="gender==0">
+    你是个男生
+  </div>
+  <div v-else-if="gender==1">
+    你是个女生
+  </div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const pTitle="vbind"
+var gender=ref()
+
+</script>
+
